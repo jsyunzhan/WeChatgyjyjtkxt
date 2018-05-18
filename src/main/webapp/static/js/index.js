9a40788684821e7d254c7968010e3af23b8df5d2
linjian <linjian@qq.com>
2018-05-18 16:09:46 +0800

    44

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,39 +1,35 @@
 $(function(){
 
 	// 图片上传
-    	$("#addImage").change(function(){
-            var imageOldNum = $(".fileImage").length;
-    		function getObjectURL(file) {  
-                var url = null;  
-                if (window.createObjcectURL != undefined) {  
-                    url = window.createOjcectURL(file);
-                } else if (window.URL != undefined) {  
-                    url = window.URL.createObjectURL(file);  
-                } else if (window.webkitURL != undefined) {  
-                    url = window.webkitURL.createObjectURL(file);  
-                }  
-                return url;
+    $("#addImage").change(function(){
+        var imageOldNum = $(".fileImage").length;
+        function getObjectURL(file) {
+            var url = null;
+            if (window.createObjcectURL != undefined) {
+                url = window.createOjcectURL(file);
+            } else if (window.URL != undefined) {
+                url = window.URL.createObjectURL(file);
+            } else if (window.webkitURL != undefined) {
+                url = window.webkitURL.createObjectURL(file);
             }
-            for(var i=0;i<4-imageOldNum;i++){
-                if (this.files[i]){
-                    //这里的objURL就是input file的真实路径
-                    var objURL = getObjectURL(this.files[i]);
-                    var imageHtml = '<div class="fileImage"><img src="'+objURL+'"></div>';
-                    $(".upload").append(imageHtml);
-                }
+            return url;
+        }
+        for(var i=0;i<4-imageOldNum;i++){
+            if (this.files[i]){
+                //这里的objURL就是input file的真实路径
+                var objURL = getObjectURL(this.files[i]);
+                var imageHtml = '<div class="fileImage"><img src="'+objURL+'"><div class="imgclose"><img src="../images/imgClose.png"></div></div>';
+                $(".upload").append(imageHtml);
             }
-            var imageNewNum = $(".fileImage").length;
-            if (imageNewNum == 4) {
-                $(".imagesNum").text(imageNewNum);
-                $(".addImage").hide();
-            }else{
-                $(".imagesNum").text(imageNewNum);
-            }
-            
-    	})
-
-	var win_height = $(window).height();
-	$(".wrapper").height(win_height-50);
+        }
+        var imageNewNum = $(".fileImage").length;
+        if (imageNewNum == 4) {
+            $(".imagesNum").text(imageNewNum);
+            $(".addImage").hide();
+        }else{
+            $(".imagesNum").text(imageNewNum);
+        }
+    })
 
     var mapObj = new AMap.Map('iCenter');
     mapObj.plugin('AMap.Geolocation', function () {
